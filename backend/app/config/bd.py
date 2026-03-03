96e5834f50da16d0d24162fd201a2031fa188e4f
ksebas20500 <kevinsebas5020@gmail.com>
2026-03-02 22:04:51 -0500

    base de datos pa render

--- backend/app/config/bd.py
+++ backend/app/config/bd.py
@@ -5,10 +5,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def obtener_conexion():
-    return psycopg2.connect(
-        host="localhost",
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port="5432"
-    )
+    # Render usará esta URL automáticamente
+    url = os.getenv("DATABASE_URL")
+    
+    if url:
+        return psycopg2.connect(url)
+    else:
+        # Esto es solo para cuando trabajes en tu PC
+        return psycopg2.connect(
+            host="localhost",
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            port="5432"
+        )
